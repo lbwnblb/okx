ef4d1eda052a6ca1f675db605f0e87a6af435383
Your Name <you@example.com>
2025-11-16 23:08:06 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sonic_rs::{JsonContainerTrait, JsonValueTrait};
 use okx::common::rest_api::instruments;
 use okx::common::utils::log_init;
 use okx::common::ws_api::create_ws;
@@ -6,8 +7,10 @@ use okx::common::ws_api::create_ws;
 async fn main() ->Result<(), Box<dyn std::error::Error>>{
     log_init();
     let value = instruments().await?;
-    value
-
-
+    let data = value.get("data").unwrap();
+    let data = data.as_array().unwrap();
+    data.iter().for_each(|item|{
+        println!("{}", item);
+    });
     Ok(())
 }
