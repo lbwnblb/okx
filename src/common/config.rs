86fea8d97b763241e8ffa10c2827a4384a2a749d
Your Name <you@example.com>
2025-11-28 23:08:27 +0800

    1

--- src/common/config.rs
+++ src/common/config.rs
@@ -1,2 +1,11 @@
+use std::sync::LazyLock;
+
 pub const WS_URL: &str = "wss://ws.okx.com:8443/ws/v5/public";
+pub const WS_SIMULATION_URL: &str = "wss://wspap.okx.com:8443/ws/v5/public";
 pub const REST_URL: &str = "https://www.okx.com";
+pub static  OKX_API_KEY:LazyLock<String> = LazyLock::new(|| std::env::var("OKX_API_KEY").expect("OKX_API_KEY not set"));
+pub static  OKX_SECRET_KEY:LazyLock<String> = LazyLock::new(|| std::env::var("OKX_SECRET_KEY").expect("OKX_SECRET_KEY not set"));
+pub static  OK_ACCESS_PASSPHRASE:LazyLock<String> = LazyLock::new(|| std::env::var("OK_ACCESS_PASSPHRASE").expect("OK_ACCESS_PASSPHRASE not set"));
+pub static  OKX_SIMULATION_API_KEY:LazyLock<String> = LazyLock::new(|| std::env::var("OKX_SIMULATION_API_KEY").expect("OKX_SIMULATION_API_KEY not set"));
+pub static  OK_SIMULATION_ACCESS_PASSPHRASE:LazyLock<String> = LazyLock::new(|| std::env::var("OK_SIMULATION_ACCESS_PASSPHRASE").expect("OK_SIMULATION_ACCESS_PASSPHRASE not set"));
+pub static  OKX_SIMULATION_SECRET_KEY:LazyLock<String> = LazyLock::new(|| std::env::var("OKX_SIMULATION_SECRET_KEY").expect("OKX_SIMULATION_SECRET_KEY not set"));
--- src/common/utils.rs
+++ src/common/utils.rs
@@ -1,15 +1,17 @@
 use std::io::Write;
 use std::ops::Add;
 use std::str::FromStr;
-use chrono::{FixedOffset, TimeZone, Utc};
+use base64::Engine;
+use base64::prelude::BASE64_STANDARD;
+use chrono::{DateTime, FixedOffset, TimeZone, Utc};
 use env_logger::Builder;
+use hmac::{Hmac, Mac};
 use log::{info, LevelFilter};
 use once_cell::sync::Lazy;
 use reqwest::{Client, Response};
 use reqwest::header::HeaderMap;
-use sonic_rs::from_str;
-use crate::common::config::REST_URL;
-
+use sha2::Sha256;
+use crate::common::config::{OKX_SIMULATION_API_KEY, OKX_SIMULATION_SECRET_KEY, OK_SIMULATION_ACCESS_PASSPHRASE, REST_URL};
 static HTTP_CLIENT:Lazy<Client> = Lazy::new(||{
         let mut headers = HeaderMap::new();
         headers.insert("Accept", "application/json".parse().unwrap());
@@ -19,6 +21,7 @@ static HTTP_CLIENT:Lazy<Client> = Lazy::new(||{
             .build()
             .expect("Failed to build HTTP client")
 });
+
 
 pub fn get_client()->&'static Client{
     &HTTP_CLIENT
@@ -36,6 +39,32 @@ impl HttpClient {
             }
             Some(params) => {
                 Ok(request_builder.query(params).send().await?)
+            }
+        }
+    }
+}
+pub struct HttpClientSimulation;
+impl HttpClientSimulation {
+    pub async fn get(path: &str, params: Option<&[(&str, &str)]>)-> Result<Response, reqwest::Error>{
+        let now_iso = utc_now_iso();
+        let client = get_client();
+        let url = format!("{REST_URL}{path}");
+        let request_builder = client.get(url.as_str());
+        let mut request_builder = request_builder.header("OK-ACCESS-KEY", OKX_SIMULATION_API_KEY.as_str())
+            .header("OK-ACCESS-TIMESTAMP", &now_iso).header("OK-ACCESS-PASSPHRASE", OK_SIMULATION_ACCESS_PASSPHRASE.as_str());
+        match params {
+            None => {
+                let sign = sign(&now_iso, "GET", path,OKX_SIMULATION_SECRET_KEY.as_str());
+                request_builder = request_builder.header("OK-ACCESS-SIGN",sign);
+                Ok(request_builder.send().await?)
+            }
+            Some(params) => {
+                let path_and_query = request_builder.try_clone().unwrap().query(params).build()?;
+                let query = path_and_query.url().query().unwrap();
+                let path_and_query = format!("{}?{}", path,query);
+                let sign = sign(&now_iso, "GET", path_and_query.as_ref(),OKX_SIMULATION_SECRET_KEY.as_str());
+                request_builder = request_builder.header("OK-ACCESS-SIGN",sign);
+                Ok(request_builder.send().await?)
             }
         }
     }
@@ -96,6 +125,23 @@ pub fn price_to_tick_int_str(price:&str, tick_size:&str)->u64{
     let result = u64::from_str(&price_split_0.add(price_split_1.as_ref())).unwrap();
     result
 
+}
+/// 返回类似 "2020-12-08T09:08:57.715Z" 的 UTC 时间字符串
+pub fn utc_now_iso() -> String {
+    // 获取当前 UTC 时间，精度到毫秒
+    let now: DateTime<Utc> = Utc::now();
+    // 格式化：固定使用 3 位毫秒 + Z
+    now.format("%Y-%m-%dT%H:%M:%S.%3fZ").to_string()
+}
+pub fn sign(timestamp:&str, method:&str, path:&str,secret_key:&str)->String{
+    let params = format!("{}{}{}", timestamp, method, path);
+    // 创建 HMAC-SHA256 实例
+    let mut mac = Hmac::<Sha256>::new_from_slice(params.as_bytes())
+        .expect("HMAC can take key of any size");
+    mac.update(secret_key.as_bytes());
+    let result = mac.finalize();
+    let code_bytes = result.into_bytes();
+    BASE64_STANDARD.encode(code_bytes)
 }
 
 #[cfg(test)]
