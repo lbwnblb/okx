eaa35fd22104deaeb249ecc624fd31392c9fde48
lbwnblbwnb <11178640+lbwnblbwnblbwnblbwnb@user.noreply.gitee.com>
2025-12-08 17:05:59 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use tokio_tungstenite::tungstenite::Message::Text;
 use tokio_tungstenite::tungstenite::{Error, Message, Utf8Bytes};
 use okx::common::config::{get_ws_public};
 use okx::common::rest_api::instruments;
-use okx::common::utils::{log_init, send_str};
+use okx::common::utils::{get_min_sz, get_sz, log_init, price_to_tick_int_str, send_str};
 use okx::common::ws_api::{create_ws, login, order, subscribe, BookData, Books, OkxMessage, Ticker, TickerData, CHANNEL_BOOKS, CHANNEL_TICKERS};
 
 #[tokio::main]
@@ -72,21 +72,55 @@ async fn main() ->Result<(), Box<dyn error::Error>>{
             }
         }
     }
-    Ok(())
 }
-pub  fn rx_ticker_data_spawn(mut rx: Receiver<TickerData>){
-    spawn(async move {
-        while let Some(item) = rx.recv().await {
-            info!("{:?}",item);
-        }
-    });
-}
+
 pub async fn rx_books_spawn(mut rx: Receiver<(Books,String,String)>){
         // let map_books: = BTreeMap::new();
+        let mut map_book_vec = HashMap::<(String,u64,u64),Vec<u64>>::new();
+
         loop {
             match rx.recv().await {
-                Some((b,id,p)) => {
-                    // b.
+                Some((b,inst_id,p)) => {
+                    // 消息的动作类型，例如 "snapshot" 或 "update"。
+                    match b.action.as_str() {
+                        "snapshot" => {
+                            // let vec_asks = book_data.asks.into_iter().map(|vec_str| (price_to_tick_int_str(vec_str.get(0).unwrap(), get_sz(inst_id).unwrap()),price_to_tick_int_str(vec_str.get(1).unwrap(), get_min_sz(inst_id).unwrap()))).collect::<Vec<(u64,u64)>>();
+                            // // let vec_bids = book_data.bids;
+                            // let max_price = vec_asks.iter().map(|(price, _)| { price }).max().unwrap();
+                            // let min_price = vec_asks.iter().map(|(price, _)| { price }).min().unwrap();
+                            // let sub_price = max_price - min_price;
+                            // println!("max_price: {}, min_price: {}, sub_price: {}", max_price, min_price, sub_price);
+                            // let mut vec_price_v = vec![0u64;(sub_price+1) as usize];
+                            // println!("{}", vec_price_v.len());
+                            // println!("{}",max_price-min_price);
+                            // vec_asks.iter().for_each(|(price,sz)| {
+                            //     println!("index: {},price:{} min_price:{} sz: {}", price-min_price, price,min_price, sz);
+                            //     vec_price_v[(price-min_price) as usize] = *sz;
+                            // });
+                            //
+                            // map_book_vec.insert(("BTC-USDT-SWAP".to_string(),min_price.clone(),max_price.clone()),vec_price_v);
+                            for b_d in b.data.into_iter() {
+                                let vec_asks = b_d.asks.into_iter().map(|vec_str| (price_to_tick_int_str(vec_str.get(0).unwrap(), get_sz(&inst_id).unwrap()), price_to_tick_int_str(vec_str.get(1).unwrap(), get_min_sz(&inst_id).unwrap()))).collect::<Vec<(u64, u64)>>();
+                                let max_price = vec_asks.iter().map(|(price, _)| { price }).max().unwrap();
+                                let min_price = vec_asks.iter().map(|(price, _)| { price }).min().unwrap();
+                                let interval = max_price - min_price;
+                                let mut vec_price = vec![0u64;(interval+1) as usize];
+                                vec_asks.iter().for_each(|(price,sz)| {
+                                        vec_price[(price-min_price) as usize] = *sz;
+                                });
+                                map_book_vec.insert((inst_id.clone(),min_price.clone(),max_price.clone()),vec_price);
+                                break
+                            }
+                        }
+                        "update" =>{
+                            for b_d in b.data.into_iter() {
+                                for (inst_id_,min_price,max_price) in map_book_vec.keys() {
+
+                                }
+                            }
+                        }
+                        _ => {}
+                    }
                 }
                 None => {
                     break;
