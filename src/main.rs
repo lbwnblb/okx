ba9a994b17cf76099efbadcaefb798ab6cb064c0
Your Name <you@example.com>
2025-12-13 15:08:44 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -39,12 +39,13 @@ impl TaskFn {
         let mut asks_key = ASKS.iter().map(|entry| { entry.key().clone() }).collect::<Vec<(String, u64, u64)>>();
         asks_key.sort_by(|(_, p1, _), (_, p2, _)| { p1.cmp(p2) });
         for (i,p1,p2) in asks_key {
-
             let ref_k_v = ASKS.get(&(i, p1, p2)).unwrap();
             let value = ref_k_v.value();
+            let mut flag_break = false;
             for j in 0..value.len() {
                 if j>=100 {
-                   return;
+                   flag_break=true;
+                   break;
                 }
                 let price = j as u64 + p1;
                 let quantity = value[j];
@@ -54,6 +55,9 @@ impl TaskFn {
                     info!("asks 价格：{} 数量：{}", price_str, quantity_str)
                 }
                 // println!("{}", );
+            }
+            if flag_break {
+                break;
             }
         }
         let mut bids_key = BIDS.iter().map(|entry| { entry.key().clone() }).collect::<Vec<(String, u64, u64)>>();
